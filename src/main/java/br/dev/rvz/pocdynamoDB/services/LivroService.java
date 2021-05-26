8be33089dc3877709667841a6383bd47ae7a5ad3
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-26 14:27:03 -0300

    Criar método para salvar dados no dynamodb

--- src/main/java/br/dev/rvz/pocdynamoDB/services/LivroService.java
+++ src/main/java/br/dev/rvz/pocdynamoDB/services/LivroService.java
@@ -1,5 +1,6 @@
 package br.dev.rvz.pocdynamoDB.services;
 
+import br.dev.rvz.pocdynamoDB.models.Livro;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class LivroService {
 
     @Autowired
     private DynamoDBMapper dynamoDBMapper;
+
+    public void cadastrarLivro(Livro livro) {
+        dynamoDBMapper.save(livro);
+    }
 }
