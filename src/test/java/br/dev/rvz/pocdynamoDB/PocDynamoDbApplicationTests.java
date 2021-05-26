b59ae342fcf43b4ce3cc230cfb19c917d39a4418
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-05-26 10:37:08 -0300

    Criar projeto spring boot

---
+++ src/test/java/br/dev/rvz/pocdynamoDB/PocDynamoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.pocdynamoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDynamoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
